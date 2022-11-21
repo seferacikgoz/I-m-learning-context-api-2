4022560b5a0beddcc4408e71bc1e8c4ae35cb8e6
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-21 14:04:54 +0300

    switch theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import "./style.css"
 
 function App() {
 
   const [theme, setTheme] = useState('light')
+
+  useEffect(() => {
+    document.body.className = theme
+  },[theme])
 
   return (
     <div>
